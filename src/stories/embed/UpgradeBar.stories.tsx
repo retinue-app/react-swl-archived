c961750ffdf7c40c168ea10dbc57325abb7879b8
Matan Lurey <matan@lurey.org>
2020-12-20 13:14:24 -0800

    Add Points, UpgradeBar.

---
+++ src/stories/embed/UpgradeBar.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta } from '@storybook/react/types-6-0';
+import { UpgradeBar, UpgradeBarProps } from './UpgradeBar';
+
+export default {
+  title: 'Embedded/Upgrade Bar',
+  component: UpgradeBar,
+} as Meta;
+
+export const Example = (args: UpgradeBarProps) => <UpgradeBar {...args} />;
+Example.args = {
+  heavyWeapon: 1,
+  personnel: 1,
+  gear: 1,
+  grenades: 1,
+};
